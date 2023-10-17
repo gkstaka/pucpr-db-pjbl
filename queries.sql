f0be4771f395911e3711f9a1d14516567985c5c1
gkstaka <63306281+gkstaka@users.noreply.github.com>
2023-10-16 23:18:25 -0300

    add queries -gkstaka

--- queries.sql
+++ queries.sql
@@ -1,14 +1,21 @@
-USE psychiatric_ hospital;
+USE psychiatric_hospital;
 
 -- 1.	Contar quantidade total de pacientes atendidos;
 
-SELECT COUNT(*) FROM pacient;
+SELECT COUNT(*) 'Total patients' FROM patient;
 
 -- 2.	Contar a quantidade de total de consultas realizadas;
-SELECT COUNT(*) FROM consultation;
+SELECT COUNT(*) 'Total consultations' FROM consultation;
 
 -- 3.	Contar a quantidade de total terapias realizadas;
-SELECT COUNT(*) FROM therapy;
+SELECT COUNT(*) 'Total therapies' FROM therapy;
 
--- 4.	Listar profissionais por ordem de salário;
+-- 5.	Listar profissionais por ordem de salário;
 SELECT * FROM professional ORDER BY salary;
+
+-- 12.	Listar todos os pacientes e os profissionais (médicos e psicólogos) que os atendem
+SELECT p.id "ID paciente", p.`name` "Nome", doc_p.id "ID médico", doc_p.`name` "Médico", psy_p.id "Id psicólogo", psy_p.`name` "Nome" FROM person AS p
+NATURAL JOIN patient AS pa
+JOIN treatment AS t ON t.patient_id = pa.id
+JOIN person AS doc_p ON t.doctor_id = doc_p.id
+LEFT JOIN person AS psy_p ON t.psychologist_id = psy_p.id; 
