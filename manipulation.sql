fe27316f723cb235c4c74f6ccde4d523412fbf1e
Gustavo Takahashi <gustavo.takahashi94@gmail.com>
2023-10-23 14:07:28 -0300

    finish routines.sql

--- manipulation.sql
+++ manipulation.sql
@@ -189,6 +189,131 @@ VALUES
 (5,5,5),
 (6,6,6);
 
+-- MORE INSERTS 
+INSERT INTO `person` (`id`,`name`,`birth_date`,`sex`,`cpf`,`zip`,`street`,`street_number`,`complement`,`neighborhood`,`city`,`state`,`country`,`phone`,`email`) 
+VALUES 
+(19,'Lucas White','1987-05-12','m','11022334456','756734','Rose Street',15,'','South End','San Diego','CA','USA','+1255123456','lucas@example.com'),
+(20,'Mia Thompson','1995-12-22','f','21123445567','987623','Tulip Lane',26,'APT 201','Central','San Francisco','CA','USA','+1425234567','mia@example.com'),
+(21,'Ella Harris','1980-09-08','f','31234556678','623187','Blossom Drive',35,'','East District','Seattle','WA','USA','+1155123756','ella@example.com'),
+(22,'Benjamin Clark','1975-01-14','m','41345667789','654128','Daisy Avenue',44,'APT 101','West Zone','Dallas','TX','USA','+1234123456','benjamin@example.com'),
+(23,'Charlotte Evans','1992-04-16','f','51456778890','897612','Lily Road',53,'','North Side','Austin','TX','USA','+1345123856','charlotte@example.com'),
+(24,'Jacob Wright','1989-10-25','m','61567889901','654298','Sunflower Street',62,'','Downtown','Boston','MA','USA','+1255123956','jacob@example.com'),
+(25,'Amelia Green','1978-06-21','f','71678900012','129876','Jasmine Way',71,'Suite 101','Old Town','New York','NY','USA','+1456123456','amelia@example.com'),
+(26,'Michael King','1990-08-03','m','81789001123','654120','Violet Lane',80,'','South Park','Philadelphia','PA','USA','+1555123056','michael@example.com'),
+(27,'Emily Hill','1982-12-15','f','91890112234','321654','Rosemary Drive',89,'APT 202','Midtown','Chicago','IL','USA','+1655123156','emily@example.com'),
+(28,'Daniel Lee','1977-03-19','m','10190123345','654312','Orchid Street',98,'','Uptown','Houston','TX','USA','+1755123256','daniel@example.com'),
+(29,'Lily Turner','1998-02-28','f','11090234456','789065','Gardenia Way',107,'','City Center','Phoenix','AZ','USA','+1855123356','lily@example.com'),
+(30,'Ethan Foster','1985-05-17','m','12190245567','876542','Lavender Avenue',116,'APT 303','Suburb','Los Angeles','CA','USA','+1955123456','ethan@example.com'),
+(31,'Sophie Reed','1979-11-11','f','13290256678','654213','Daffodil Drive',125,'','Countryside','Denver','CO','USA','+2055123556','sophie@example.com'),
+(32,'Jack Robinson','1993-09-23','m','14390267789','987651','Cherry Blossom Way',134,'','Metropolitan','Nashville','TN','USA','+2155123656','jack@example.com'),
+(33,'Isla Baker','1987-04-29','f','15490278890','345678','Lilac Road',143,'Suite 102','Urban','Indianapolis','IN','USA','+2255123756','isla@example.com'),
+(34,'Oscar Cooper','1996-08-08','m','16590289901','234567','Pansy Street',152,'','Coastal','Miami','FL','USA','+2355123856','oscar@example.com'),
+(35,'Isabelle Ward','1983-01-03','f','17690300012','123456','Petunia Lane',161,'','Rural','Columbus','OH','USA','+2455123956','isabelle@example.com'),
+(36,'Charlie Lewis','1994-10-13','m','18790311123','567890','Dahlia Drive',170,'APT 204','Historic','Charlotte','NC','USA','+2555124056','charlie@example.com'),
+(37,'Ava Gray','1988-07-24','f','19890322234','765432','Marigold Street',179,'','Downtown','Jacksonville','FL','USA','+2655124156','ava@example.com'),
+(38,'George Wood','1986-12-06','m','20990333345','123789','Magnolia Avenue',188,'','Suburban','Detroit','MI','USA','+2755124256','george@example.com');
+
+INSERT INTO `professional` (`id`,`enrollment`,`salary`,`start_date`,`working_range`,`speciality`,`consultation_fee`) 
+VALUES 
+(19,'LUCW78901',11500,'2023-10-20','Senior','Pulmonology',110),
+(20,'MIAT23456',11800,'2023-10-21','Mid-Level','Urology',95),
+(21,'ELLAH7890',9700,'2023-10-22','Junior','Hematology',80),
+(22,'BENJ9801',12000,'2023-10-23','Senior','Radiology',105),
+(23,'CHAR5678',9900,'2023-10-24','Mid-Level','Endocrinology',90),
+(24,'JACW4567',11600,'2023-10-25','Senior','Allergology',115),
+(25,'AMG6789',9800,'2023-10-26','Junior','Rheumatology',85),
+(26,'MIK2345',11300,'2023-10-27','Mid-Level','Nephrology',100),
+(27,'EMH1234',11400,'2023-10-28','Senior','Otorhinolaryngology',110),
+(28,'DANL5678',9600,'2023-10-29','Junior','Geriatrics',80);
+
+INSERT INTO `doctor` (`id`,`crm`) 
+VALUES 
+(19,'4567890'),
+(20,'5678901'),
+(21,'6789012'),
+(22,'7890123'),
+(23,'8901234');
+
+INSERT INTO `psychologist` (`id`,`crp`) 
+VALUES 
+(24,'asdf0123'),
+(25,'qwer1234'),
+(26,'zxcv2345'),
+(27,'erty3456'),
+(28,'fghj4567');
+
+INSERT INTO `patient` (`id`,`weight`,`marital_status`,`profession`,`emergency_contact_name`,`emergency_contact_phone`,`health_insurance`,`hospitalization_date`) 
+VALUES 
+(29,65,'Single','Engineer','Liam Turner','+1855123357','Blue Shield','2023-01-10'),
+(30,72,'Married','Musician','Eva Foster','+1955123457','Aetna','2023-02-11'),
+(31,59,'Divorced','Teacher','Evan Reed','+2055123557','Kaiser Permanente','2023-03-12'),
+(32,78,'Single','Architect','Grace Robinson','+2155123657','Cigna','2023-04-13'),
+(33,66,'Widowed','Nurse','Noah Baker','+2255123757','UnitedHealth','2023-05-14'),
+(34,70,'Married','Programmer','Zoe Cooper','+2355123857','Humana','2023-06-15'),
+(35,60,'Single','Dentist','Oliver Ward','+2455123957','Anthem','2023-07-16'),
+(36,75,'Married','Physicist','Emma Lewis','+2555124057','Molina Healthcare','2023-08-17'),
+(37,64,'Divorced','Artist','William Gray','+2655124157','Health Net','2023-09-18'),
+(38,73,'Single','Scientist','Mia Wood','+2755124257','Centene Corporation','2023-10-19');
+
+
+INSERT INTO `consultation` (`id`,`time`,`patient_id`, `doctor_id`) 
+VALUES 
+(7,'2023-11-01 00:00:00',29,1),
+(8,'2023-11-02 00:00:00',30,2),
+(9,'2023-11-03 00:00:00',31,3),
+(10,'2023-11-04 00:00:00',32,4),
+(11,'2023-11-05 00:00:00',33,5),
+(12,'2023-11-06 00:00:00',34,6),
+(13,'2023-11-07 00:00:00',35,19),
+(14,'2023-11-08 00:00:00',36,20),
+(15,'2023-11-09 00:00:00',37,21),
+(16,'2023-11-10 00:00:00',38,22),
+(17,'2023-11-11 00:00:00',13,23),
+(18,'2023-11-12 00:00:00',14,6),
+(19,'2023-11-13 00:00:00',15,1),
+(20,'2023-11-14 00:00:00',16,2),
+(21,'2023-11-15 00:00:00',17,3),
+(22,'2023-11-16 00:00:00',18,4),
+(23,'2023-11-17 00:00:00',29,5),
+(24,'2023-11-18 00:00:00',30,6),
+(25,'2023-11-19 00:00:00',31,1),
+(26,'2023-11-20 00:00:00',32,2);
+
+INSERT INTO `suggestion` (`id`, `medicine_id`, `dosage_id`, `medical_record_id`)
+VALUES
+    (7, 1, 4, 6),
+    (8, 2, 2, 2),
+    (9, 3, 6, 5),
+    (10, 3, 5, 1),
+    (11, 4, 5, 3),
+    (12, 5, 4, 2),
+    (13, 6, 2, 6),
+    (14, 4, 3, 5),
+    (15, 6, 1, 3),
+    (16, 1, 3, 4),
+    (17, 2, 5, 1);
+    
+INSERT INTO `treatment` (`id`,`name`,`start_date`,`planned_end_date`,`patient_id`) 
+VALUES 
+(7,'Obsessive-compulsive disorder treatment','2022-06-01 00:00:00','2023-06-01 00:00:00',29),
+(8,'Post-traumatic stress disorder treatment','2023-11-20 00:00:00','2024-11-20 00:00:00',30),
+(9,'Eating disorders treatment','2021-04-05 00:00:00','2022-04-05 00:00:00',31),
+(10,'Addiction treatment','2019-12-01 00:00:00','2020-12-01 00:00:00',32),
+(11,'Dissociative disorders treatment','2020-07-07 00:00:00','2021-07-07 00:00:00',33),
+(12,'Sleep disorders treatment','2022-11-01 00:00:00','2023-11-01 00:00:00',34),
+(13,'Somatic symptom disorders treatment','2021-08-15 00:00:00','2022-08-15 00:00:00',35),
+(14,'Elimination disorders treatment','2023-10-05 00:00:00','2024-10-05 00:00:00',36),
+(15,'Gender dysphoria treatment','2022-02-20 00:00:00','2023-02-20 00:00:00',37),
+(16,'Impulse control disorders treatment','2021-06-12 00:00:00','2022-06-12 00:00:00',38),
+(17,'Substance-related disorders treatment','2020-03-05 00:00:00','2021-03-05 00:00:00',29),
+(18,'Neurocognitive disorders treatment','2019-10-01 00:00:00','2020-10-01 00:00:00',30),
+(19,'Personality disorders treatment','2023-02-15 00:00:00','2024-02-15 00:00:00',31),
+(20,'Paraphilic disorders treatment','2021-09-09 00:00:00','2022-09-09 00:00:00',32),
+(21,'Factitious disorders treatment','2019-05-05 00:00:00','2020-05-05 00:00:00',33);
+
+
+
+
 /*-------------------------UPDATES-------------------------*/
 
 UPDATE treatment SET start_date = NOW(), planned_end_date = "2025-05-03" WHERE id = 2;
@@ -200,6 +325,10 @@ UPDATE dosage SET dose_frequency = "every 8 hours", dose_quantity = "150mg" WHER
 UPDATE professional SET salary = 15000 WHERE id = 4;
 
 UPDATE medical_record SET record_date = "2022-04-11" WHERE id = 2;
+
+UPDATE therapy SET capacity = 5 WHERE id = 6;
+
+UPDATE medical_record SET record_date = "2024-08-01" WHERE id = 3;
 
 /*-------------------------DELETES-------------------------*/
 
--- routines.sql
+++ routines.sql
@@ -1,5 +1,6 @@
 USE psychiatric_hospital;
 -- Procedures
+
 -- 1. Inserir um novo paciente
 DROP PROCEDURE IF EXISTS insert_patient;
 DELIMITER $$
@@ -66,14 +67,162 @@ CREATE PROCEDURE hospitalization_proportion (IN _year INT)
 	BEGIN
 		DECLARE total INT;
     SET total := (SELECT COUNT(id) FROM patient);
-    IF total != 0 THEN
+    IF total != 0 THEN -- para evitar divisoes por 0
 		SELECT COUNT(*)/total AS "Distribution" , MONTH(hospitalization_date) "Month" FROM patient 
 		WHERE YEAR(hospitalization_date) = _year
         GROUP BY MONTH(hospitalization_date)
 		ORDER BY MONTH(hospitalization_date);
-	ELSE SELECT total "Total";
+	ELSE SELECT "NaN";
 	END IF;
     END $$
 DELIMITER ;
 
 CALL hospitalization_proportion(2023);
+
+-- 3. Tentar vincular um paciente a uma terapia se estiver dentro da capacidade dela
+DROP PROCEDURE IF EXISTS insert_mr_to_therapy;
+DELIMITER $$
+CREATE PROCEDURE insert_mr_to_therapy (IN _medical_record_id INT, IN _therapy_id INT)
+	BEGIN
+		-- busca a capacidade
+        DECLARE cap INT;
+        DECLARE cnt INT;
+        SET cap := (SELECT capacity FROM therapy WHERE id = _therapy_id);
+        SET cnt := (SELECT COUNT(*) FROM medical_record_included_therapy WHERE therapy_id = _therapy_id);
+        IF cnt < cap THEN
+			INSERT INTO medical_record_included_therapy VALUES (NULL, _medical_record_id, _therapy_id);
+            SET cnt := (SELECT COUNT(*) FROM medical_record_included_therapy WHERE therapy_id = _therapy_id);
+			SELECT cap, cnt;
+		ELSE 
+			SELECT CONCAT("No vacancy for therapy id = ", _therapy_id) "Warning";
+        END IF;
+    END $$
+DELIMITER ;
+
+CALL insert_mr_to_therapy(1,6);
+
+-- Functions
+
+-- 1.Retornar o número de consultas de um determinado médico realizado no período do último ano
+DROP FUNCTION IF EXISTS n_consultation_doctor;
+DELIMITER $$
+CREATE FUNCTION n_consultation_doctor (_id INT) RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE n_consultation INT;
+	SET n_consultation := (SELECT COUNT(*) FROM consultation 
+						WHERE doctor_id = _id AND `time` BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW() );
+	RETURN n_consultation;
+END $$
+DELIMITER ;
+SELECT n_consultation_doctor(4) "Consultations over the last year";
+
+-- 2. Retorna a quantidade de atualizações (médicos e psicólogos) do prontuário de um determinado paciente
+DROP FUNCTION IF EXISTS count_updates;
+DELIMITER $$
+CREATE FUNCTION count_updates (_patient_id INT) RETURNS INT DETERMINISTIC
+	BEGIN
+		DECLARE mr_id, cnt INT;
+        SET mr_id := (SELECT id FROM medical_record WHERE patient_id = _patient_id);
+        SET cnt := (SELECT COUNT(*) FROM doctor_update_record WHERE medical_record_id = mr_id);
+        SET cnt := cnt + (SELECT COUNT(*) FROM psychologist_update_record WHERE medical_record_id = mr_id);
+        RETURN cnt;
+    END $$
+DELIMITER ;
+SELECT count_updates(13) "Total updates in medical record";
+
+-- 3.	Calcular a progressão do tratamento baseado na data de início, a data da última atualização do prontuário e a data planejada do fim do tratamento
+DROP FUNCTION IF EXISTS progression;
+DELIMITER $$
+CREATE FUNCTION progression (_medical_record_id INT) RETURNS FLOAT DETERMINISTIC
+	BEGIN
+    -- DECLARE pt_id INT;
+    DECLARE t_id INT;
+    DECLARE str_date, last_update_date, finish_date DATE;
+    DECLARE result FLOAT;
+    -- SET pt_id := (SELECT patient_id FROM medical_record WHERE id = _medical_record_id);
+    SET t_id := (SELECT treatment_id FROM medical_record WHERE id = _medical_record_id);
+    /*SET str_date := (SELECT DATE(start_date) FROM treatment WHERE id = t.id);
+    SET finish_date := (SELECT DATE(start_date) FROM treatment WHERE id = t.id);*/
+    SELECT DATE(start_date), DATE(planned_end_date) INTO str_date, finish_date FROM treatment WHERE id = t_id;
+    SET last_update_date := (SELECT record_date FROM medical_record WHERE id = _medical_record_id);
+    SET result := (
+		CASE
+			WHEN last_update_date < str_date THEN 0
+            WHEN last_update_date > finish_date THEN 1
+            ELSE DATEDIFF(last_update_date, str_date)/DATEDIFF(finish_date, str_date)
+		END
+	);
+    RETURN result;
+    END $$
+DELIMITER ;
+SELECT CONCAT(progression(1)*100, "%") "Progression";
+
+-- Triggers 
+
+-- 1. Atualizar o prontuário médico após a criação de um novo doctor_update record ou psychologist_update_record com o tempo atual
+DROP TRIGGER IF EXISTS update_time_doctor;
+DELIMITER $$
+CREATE TRIGGER update_time_doctor AFTER INSERT ON doctor_update_record
+	FOR EACH ROW
+		BEGIN
+			UPDATE medical_record SET record_date = NOW() WHERE id = NEW.medical_record_id;
+        END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS update_time_psychologist;
+DELIMITER $$
+CREATE TRIGGER update_time_psychologist AFTER INSERT ON psychologist_update_record
+	FOR EACH ROW
+		BEGIN
+			UPDATE medical_record SET record_date = NOW() WHERE id = NEW.medical_record_id;
+        END $$
+DELIMITER ;
+
+SELECT * FROM (doctor_update_record);
+SELECT * FROM medical_record;
+INSERT INTO doctor_update_record VALUES (NULL, 4, 6);
+SELECT * FROM (doctor_update_record);
+SELECT * FROM medical_record;
+
+-- 2. Deletar profissional apagando todos os outros registros que possam causar erro de constraint
+DROP TRIGGER IF EXISTS delete_professional;
+DELIMITER $$
+CREATE TRIGGER delete_professional BEFORE DELETE ON professional
+	FOR EACH ROW
+	BEGIN
+		IF EXISTS (SELECT 1 FROM doctor WHERE id = OLD.id) THEN
+			DELETE FROM consultation WHERE doctor_id = OLD.id;
+			DELETE FROM doctor_suggest_treatment WHERE doctor_id = OLD.id;
+			DELETE FROM doctor_update_record WHERE doctor_id = OLD.id;
+			DELETE FROM doctor WHERE id = OLD.id;
+        ELSEIF (SELECT 1 FROM psychologist WHERE id = OLD.id) THEN
+			DELETE FROM psychologist_helps_treatment WHERE psychologist_id = OLD.id;
+            DELETE FROM psychologist_update_record WHERE psychologist_id = OLD.id;
+            DELETE FROM medical_record_included_therapy WHERE therapy_id = (SELECT id FROM therapy WHERE psychologist_id = OLD.id);
+            DELETE FROM therapy WHERE psychologist_id = OLD.id;
+            DELETE FROM psychologist WHERE id = OLD.id;
+		END IF;	
+    END $$
+DELIMITER ;
+
+SELECT id FROM doctor;
+SELECT id FROM psychologist;
+DELETE FROM professional WHERE id = 3;
+DELETE FROM professional WHERE id = 12;
+-- 3. Corrigir data do fim do tratamento para a ultima atualização no prontuário caso a atualização do prontuário seja maior que o fim
+DROP TRIGGER IF EXISTS update_planned_end_date;
+DELIMITER $$
+CREATE TRIGGER update_planned_end_date AFTER UPDATE ON medical_record
+	FOR EACH ROW
+    BEGIN
+		DECLARE t_id INT;
+		DECLARE treatment_end_date DATETIME;
+        SET t_id := OLD.treatment_id;
+        SET treatment_end_date := (SELECT planned_end_date FROM treatment WHERE id = t_id);
+		IF treatment_end_date < CAST(NEW.record_date AS DATETIME) THEN
+			UPDATE treatment SET planned_end_date = NEW.record_date WHERE id = t_id;
+        END IF ;
+    END $$
+DELIMITER ;
+SELECT * FROM treatment;
+UPDATE medical_record SET record_date = "2030-01-01" WHERE id = 1;
